829ef5b0309bae36d5112fe55da6529ba19a6650
Gyuh0407 <202021123075@jmu.edu.cn>
2022-08-23 16:28:14 +0800

    version 3 无报错但 出现的问题是app闪退

--- app/src/main/java/com/example/testdemo/MyRecyclerViewAdapter.java
+++ app/src/main/java/com/example/testdemo/MyRecyclerViewAdapter.java
@@ -1,60 +1,75 @@
 package com.example.testdemo;
 
 import android.content.Context;
+import android.graphics.drawable.Drawable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import android.widget.ImageView;
 import android.widget.TextView;
 
+import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 
-import java.util.ArrayList;
+
 
 import java.util.List;
-import java.util.Map;
 
-public class MyRecyclerViewAdapter extends RecyclerView.Adapter<MyRecyclerViewAdapter.RecyclerHolder> {
+public class MyRecyclerViewAdapter extends RecyclerView.Adapter<MyRecyclerViewAdapter.MyViewHolder> {
     public Context mContext;
-    public List<Data> mDatas = new ArrayList<>();
-    public Data mData;
-
-
-    public MyRecyclerViewAdapter(MainActivity recyclerView, List<Map<String, Object>> dataList) {
-        recyclerView.getContext();
-    }
+    public List<List<String>> mList;
 
 
 
-    @Override
-    public RecyclerHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.simplesdapteractivity,null,false);
-        return new RecyclerHolder(view);
+
+
+
+    public MyRecyclerViewAdapter(Context mContext, List<List<String>> mList) {
+        this.mContext = mContext;
+        this.mList = mList;
 
     }
 
+
+    @NonNull
     @Override
-    public void onBindViewHolder(RecyclerHolder holder, int position) {
-        if (holder instanceof  ViewHolde){
+    public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View itemView = LayoutInflater.from(mContext).inflate(R.layout.simplesdapteractivity, parent, false);
+        MyViewHolder holder = new MyViewHolder(itemView);
+        return holder;
+    }
 
-            holder.textView.setText(Data.get(position));
-
-        }
+    @Override
+    public void onBindViewHolder(MyViewHolder holder, int position) {
+        holder.iv1.setImageDrawable((Drawable) mList.get(position));
+        holder.tv1.setText((CharSequence) mList.get(position));
+        holder.tv2.setText((CharSequence) mList.get(position));
+        holder.tv3.setText((CharSequence) mList.get(position));
     }
 
     @Override
     public int getItemCount() {
-        return Data.size();
+        return mList.size();
+
     }
 
-    class RecyclerHolder extends RecyclerView.ViewHolder {
-       TextView textView;
 
-        private RecyclerHolder(View itemView) {
+    class MyViewHolder extends RecyclerView.ViewHolder {
+        private ImageView iv1;
+        private TextView tv1;
+        private TextView tv2;
+        private TextView tv3;
+
+
+        public MyViewHolder(View itemView) {
             super(itemView);
-
-
+            iv1 = itemView.findViewById(R.id.img_star);
+            tv1 = itemView.findViewById(R.id.person_name);
+            tv2 = itemView.findViewById(R.id.person_score);
+            tv3 = itemView.findViewById(R.id.person_local);
         }
     }
+
 }
